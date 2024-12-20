4e9d1523a3b88b6e069b187e4354c73c0bf4e537
Tom Russell <tom.russell@ouce.ox.ac.uk>
2024-12-20 15:50:28 +0000

    Handle version number and rate limit/retry in metadata script

--- scripts/published_metadata.py
+++ scripts/published_metadata.py
@@ -11,12 +11,13 @@ from glob import glob
 
 import requests
 import json
+from tqdm import tqdm
 
 if __name__ == "__main__":
     base_path = os.path.join(os.path.dirname(__file__), "..")
-    deposition_fnames = glob(os.path.join(base_path, "zenodo", "*.json"))
+    deposition_fnames = glob(os.path.join(base_path, "zenodo", "*.deposition.json"))
     depositions = []
-    for fname in sorted(deposition_fnames):
+    for fname in tqdm(sorted(deposition_fnames), desc="Reading"):
         with open(fname, "r") as fh:
             dep = json.load(fh)
             dep["fname"] = fname
@@ -27,19 +28,30 @@ if __name__ == "__main__":
         writer.writerow(
             ("zenodo_deposition_id", "country", "title", "doi", "url", "citation")
         )
-        for dep in depositions:
+        for dep in tqdm(depositions, desc="Querying and writing"):
             dep_id = dep["id"]
-            r = requests.get(f"https://zenodo.org/api/records/{dep_id}")
-            print(r.status_code, dep["fname"])
-            if r.status_code != 200:
-                continue
-            pub = r.json()
+            retries = 0
+            max_retry = 5
+            while retries < max_retry:
+                try:
+                    r = requests.get(f"https://zenodo.org/api/records/{dep_id}")
+                    if r.status_code != 200:
+                        print(r.status_code, r.text)
+                        time.sleep(1)
+                    r.raise_for_status()
+                except Exception:
+                    continue
+
+                pub = r.json()
+                break
+
             title = pub["title"]
             doi = pub["doi"]
-            citation = f"Russell, T., Jaramillo, D., Nicholas, C., Thomas, F., Pant, R., & Hall, J. W. (2023). {title} (0.1.0) [Data set]. Zenodo. https://doi.org/{doi}"
+            version = pub["metadata"]["version"]
+            citation = f"Russell, T., Jaramillo, D., Nicholas, C., Thomas, F., Pant, R., & Hall, J. W. (2023). {title} ({version}) [Data set]. Zenodo. https://doi.org/{doi}"
             country = title.replace(
                 "Infrastructure Climate Resilience Assessment Data Starter Kit for ", ""
             )
             url = f"https://zenodo.org/records/{dep_id}"
             writer.writerow((dep_id, country, title, doi, url, citation))
-            time.sleep(1)
+            time.sleep(0.5)  # respect rate-limit of ~120 per minute
